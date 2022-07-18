c42e26a0f2433d3c661a4084429104699c4f1520
LuisGomes18 <97010136+LuisGomes18@users.noreply.github.com>
2022-07-18 03:27:23 +0100

    Add files via upload

--- Pedra papel ou tesoura (player vs pc).py
+++ Pedra papel ou tesoura (player vs pc).py
@@ -5,6 +5,8 @@ def acertou():
 
 def errou():
     print('Errou')
+pontuacaojogador  =  0
+pontuacaopc = 0 
 
 jogo = ["pedra", "papel", "tesoura"]
 n = 0
@@ -12,11 +14,21 @@ while n < 3:
     n += 1
     resultadopc = random.choice(jogo)
 
-    resultadousuario = str(input('Qual sua jogada: '))
-
-    print(resultadopc)
+    resultadousuario = str(input('\n Qual sua jogada: '))
+    print(f'Joga do pc foi: {resultadopc}')
 
     if resultadopc == resultadousuario:
         acertou()
+        pontuacaojogador += 1
     elif resultadopc != resultadousuario:
         errou()
+        pontuacaopc +=1
+
+print(f'\n\033[42m'+'\033[1m'+'\033[33m'+'\nPontuação total'+ '\033[0;0m')
+print(f'Jogador 1: {pontuacaojogador} pontos\nJogador 2: {pontuacaopc} pontos\n')
+if pontuacaojogador == pontuacaopc:
+ print('Empate')
+elif pontuacaojogador > pontuacaopc:
+ print('Quem ganhou foi o ' + '\u001b[33m' + '\033[1m'  + 'Jogador' + '\033[0;0m')
+elif pontuacaojogador < pontuacaopc:
+ print('Quem ganhou foi o ' + '\u001b[33m' + '\033[1m'  + 'PC' + '\033[0;0m')
--- Pedra papel ou tesoura (player vs player).py
+++ Pedra papel ou tesoura (player vs player).py
@@ -1,50 +1,53 @@
 n = 0
 pontuacaoplayer1 = 0
 pontuacaoplayer2 = 0
-def jogador1 ():
-    print('Jogador 1 marca ponto \n')
-def jogador2 ():
-    print('Jogador 2 marca ponto \n')    
+def jg1 ():
+    print(f'{jogador1} marca ponto \n')
+def jg2 ():
+    print(f'{jogador2} marca ponto \n')    
+jogador1 = str(input('Insira o nome do primeiro jogador: '))
+jogador2 = str(input('Insira o nome do segundo jogador: '))
 
 while n < 3:
     n += 1
-    escolhaplayer1 = str(input('Jogador 1 suavez: '))
-    escolhaplayer2 = str(input('Jogador 2 sua vez: '))
+    escolhaplayer1 = str(input(f'\n{jogador1} sua vez: '))
+    escolhaplayer2 = str(input(f'{jogador2} sua vez: '))
 
     if escolhaplayer1 == "pedra":
         if escolhaplayer2 == "tesoura":
-            jogador1 ()
+            jg1 ()
             pontuacaoplayer1 += 1
 
     if escolhaplayer1 == "tesoura":
         if escolhaplayer2 == "papel":
-            jogador1 ()
+            jg1 ()
             pontuacaoplayer1 +=1
 
     if escolhaplayer1 == "papel":
         if escolhaplayer2 == "pedra":
-            jogador1 ()
+            jg1 ()
             pontuacaoplayer1 += 1
 
     if escolhaplayer2 == "pedra":
         if escolhaplayer1 == "tesoura":
-            jogador2 ()
+            jg2 ()
             pontuacaoplayer2 += 1
 
     if escolhaplayer2 == "tesoura":
         if escolhaplayer1 == "papel":
-            jogador2 ()
+            jg2 ()
             pontuacaoplayer2 += 1
 
     if escolhaplayer2 == "papel":
         if escolhaplayer1 == "pedra":
-            jogador2 ()        
+            jg2 ()        
             pontuacaoplayer2 += 1
 
-print(f'\nPontuação total\nJogador 1- {pontuacaoplayer1} pontos\nJogador 2- {pontuacaoplayer2} pontos')
+print(f'\033[42m'+'\033[1m'+'\033[33m'+'\nPontuação total'+ '\033[0;0m')
+print(f'Jogador 1: {pontuacaoplayer1} pontos\nJogador 2: {pontuacaoplayer2} pontos\n')
 if pontuacaoplayer1 == pontuacaoplayer2:
-    print('Empate')
+ print('Empate')
 elif pontuacaoplayer1 > pontuacaoplayer2:
-    print('Quem ganhou foi o ' + '\u001b[33m' + 'Jogador 1')
+ print('Quem ganhou foi o ' + '\u001b[33m' + '\033[1m'  + f'{jogador1}' + '\033[0;0m')
 elif pontuacaoplayer1 < pontuacaoplayer2:
-    print('Quem ganhou foi o ' + '\u001b[33m' + 'Jogador 2')
+ print('Quem ganhou foi o ' + '\u001b[33m' + '\033[1m'  + f'{jogador2}' + '\033[0;0m')
