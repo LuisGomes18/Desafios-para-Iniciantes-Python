9ac66674a7f333d0f2841c9f412a6890e6085297
LuisGomes18 <luiscogomes@edu.madeira.gov.pt>
2023-03-19 11:59:23 +0000

    add files via upload

---
+++ Adivinhar o Numero (PC vs PC)/extra.py
@@ -0,0 +1,7 @@
+Questoes_Certas_PC_1 = 0
+Questoes_Certas_PC_2 = 0
+
+
+def Pontuacao():
+    print('\033[93m' + 'Pontuacao Final' + '\033[0m')
+    print(f'PC 1 fez {Questoes_Certas_PC_1}\n PC 2 fez {Questoes_Certas_PC_2}')
