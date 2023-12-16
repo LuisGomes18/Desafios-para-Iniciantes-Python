abe33c04c3b2c880d0d5d11624718d8000432432
LuisGomes18 <luiscgomes0@gmail.com>
2023-12-16 22:14:37 +0000

    Update do Pedra, Papel ou Tesoura (PC VS PC)

--- Pedra,Papel ou Tesoura (PC vs PC)/main.py
+++ Pedra,Papel ou Tesoura (PC vs PC)/main.py
@@ -1,47 +1,32 @@
-from random import choices
+from random import choice
 
-acoes_permitidas = ['pedra', 'papel', 'tesoura']
+movimentos = ['pedra', 'papel', 'tesoura']
 ponto_AI_1 = 0
 ponto_AI_2 = 0
 n = 0
 
-amarelo = '\033[33m'
-original = '\033[0;0m'
-negrito = '\033[1m'
-
-def ponto_ai_1():
-    global ponto_AI_1
-    print(amarelo + 'Ponto da AI 1\n'+ original)
-    ponto_AI_1 += 1
-
-def ponto_ai_2():
-    global ponto_AI_2
-    print(amarelo + 'Ponto da AI 2\n'+ original)
-    ponto_AI_2 += 1
-
 while n < 5:
-    movimento_AI_1 = str(choices(acoes_permitidas))
-    movimento_AI_2 = str(choices(acoes_permitidas))
-    movimento_AI_1 = movimento_AI_1.replace("[", "").replace("]", "").replace("'", "")
-    movimento_AI_2 = movimento_AI_2.replace("[", "").replace("]", "").replace("'", "")
-    print(f'AI 1 escolheu {movimento_AI_1}\nAI 2 escolheu {movimento_AI_2}')
-    if movimento_AI_1 == movimento_AI_2:
-        print(amarelo + 'Empatou\n' + original)
-
-    elif movimento_AI_1 == 'pedra' and movimento_AI_2 == 'tesoura':
-        ponto_ai_1()
-    elif movimento_AI_1 == 'tesoura' and movimento_AI_2 == 'papel':
-        ponto_ai_1()
-    elif movimento_AI_1 == 'papel' and movimento_AI_2 == 'pedra':
-        ponto_ai_1()
+    movimento_AI_1 = choice(movimentos)
+    movimento_AI_2 = choice(movimentos)
     
-    elif movimento_AI_2 == 'pedra' and movimento_AI_1 == 'tesoura':
-        ponto_ai_2()
-    elif movimento_AI_2 == 'tesoura' and movimento_AI_1 == 'papel':
-        ponto_ai_2()
-    elif movimento_AI_2 == 'papel' and movimento_AI_1 == 'pedra':
-        ponto_ai_2()
+    print(f'\nAI 1 escolheu {movimento_AI_1}')
+    print(f'AI 2 escolheu {movimento_AI_2}\n')
+    if movimento_AI_1 == movimento_AI_2:
+        print('Empatou')
+    elif movimento_AI_1 == 'pedra' and movimento_AI_2 == 'tesoura':
+        print('Ponto para AI 1')
+        ponto_AI_1 += 1
+    elif movimento_AI_1 == 'papel' and movimento_AI_2 == 'pedra':
+        print('Ponto para AI 1')
+        ponto_AI_1 += 1
+    elif movimento_AI_1 == 'tesoura' and movimento_AI_2 == 'papel':
+        print('Ponto para AI 1')
+        ponto_AI_1 += 1
+    else:
+        print('Ponto para AI 2')
+        ponto_AI_2 += 1
+    print('\n')
     n += 1
 
-print(negrito + amarelo + 'Pontuacao final' + original)
+print('\nPontuacao Final')
 print(f'AI 1 fez {ponto_AI_1}\nAI 2 fez {ponto_AI_2}')
