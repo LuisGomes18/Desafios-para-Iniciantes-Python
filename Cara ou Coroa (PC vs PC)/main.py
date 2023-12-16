8b1b5171c95373053b889c0b06ca6b5b340657f8
LuisGomes18 <luiscgomes0@gmail.com>
2023-12-16 19:50:35 +0000

    Update do Cara VS Coroa (PC VS PC)

--- Cara ou Coroa (PC vs PC)/main.py
+++ Cara ou Coroa (PC vs PC)/main.py
@@ -1,19 +1,20 @@
 from random import choice
 
-movimentos = ['cara', 'coroa']
-n = 0
 
-while n == 0:
-    movimento = choice(movimentos)
-    jogada_ai_1 = choice(movimentos)
-    jogada_ai_2 = choice(movimentos)
-    print(f'Caiu {movimento}')
-    if movimento == jogada_ai_1 and movimento != jogada_ai_2:
-        print(f'\nAI 1 escolheu {jogada_ai_1} e AI 2 escolheu {jogada_ai_2}')
-        print('AI 1 acertou')
-        print('AI 2 errou')
-    elif movimento == jogada_ai_2 and movimento != jogada_ai_1:
-        print(f'\nAI 1 escolheu {jogada_ai_1} e AI 2 escolheu {jogada_ai_2}')
-        print('AI 2 acertou')
-        print('AI 1 errou')
-    n = 1
+movimentos = ['cara', 'coroa']
+
+movimento_geral = choice(movimentos)
+jogada_AI_1 = choice(movimentos)
+movimentos.remove(jogada_AI_1)
+jogada_AI_2 = choice(movimentos)
+
+print(f'AI 1 escolheu {jogada_AI_1}')
+print(f'AI 2 escolheu {jogada_AI_2}')
+print(f'\nCaiu {movimento_geral}')
+
+if jogada_AI_1 == movimento_geral:
+    print('\nAI 1 acertou')
+    print('AI 2 errou')
+elif jogada_AI_2 == movimento_geral:
+    print('\nAI 2 acertou')
+    print('AI 1 errou')
