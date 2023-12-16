5f5f57d003913729a76dca1e9474d24a200c37f7
LuisGomes18 <luiscgomes0@gmail.com>
2023-12-16 19:18:18 +0000

    Update do codigo do Adivinhar o NUmero (PC VS UTILIZADOR)

--- Adivinhar o Numero (PC vs USUARIO)/mian.py
+++ Adivinhar o Numero (PC vs USUARIO)/mian.py
@@ -1,31 +1,33 @@
 from random import randint
 
-n = 0
-pontos_AI_1 = 0
-pontos_Utilizador = 0
 
+N = 0
+PONTOS_AI_1 = 0
+PONTOS_UTILIZADOR = 0
 
-def verificacao(numero_escolhido_AI_1, numero_Utilizador, pontos_AI_1, pontos_Utilizador):
-    if numero_escolhido_AI_1 == numero_Utilizador:
+while N < 5:
+    numero_escolhido_AI_1 = randint(1, 10)
+    numero_escolhido_Usuario = int(input('Insira um numero: (1 a 10): '))
+    while numero_escolhido_Usuario not in range(1, 11):
+        numero_escolhido_Usuario = int(input('Insira um numero: (1 a 10): '))
+
+    print(f'\nNúmero escolhido por AI 1: {numero_escolhido_AI_1}')
+    print(f'Número escolhido por AI 2: {numero_escolhido_Usuario}')
+
+    if numero_escolhido_AI_1 == numero_escolhido_Usuario:
         print('Empate')
-    elif numero_escolhido_AI_1 > numero_Utilizador:
-        print('Ponto para AI 2')
-        pontos_Utilizador += 1
-    elif numero_escolhido_AI_1 < numero_Utilizador:
+    elif numero_escolhido_AI_1 > numero_escolhido_Usuario:
         print('Ponto para AI 1')
-        pontos_AI_1 += 1
+        PONTOS_AI_1 += 1
+    elif numero_escolhido_AI_1 < numero_escolhido_Usuario:
+        print('Ponto para AI 2')
+        PONTOS_UTILIZADOR += 1
 
-    return pontos_AI_1, pontos_Utilizador
+    print(f'Pontos do AI 1: {PONTOS_AI_1}')
+    print(f'Pontos do Utilizador: {PONTOS_UTILIZADOR}\n')
+    N += 1
 
 
-while n < 5:
-    numero_escolhido_AI_1 = randint(0, 10)
-    numero_Utilizador = int(input('Escreva o numero: '))
-    pontos_AI_1, pontos_AI_2 = verificacao(numero_escolhido_AI_1, numero_Utilizador, pontos_AI_1, pontos_Utilizador)
-    print('\033[32m' + f'Pontos do AI 1: {pontos_AI_1}')
-    print(f'Pontos do Utilizador: {pontos_Utilizador}\n' + '\033[0m')
-    n += 1
-
-print('\033[1m' + '\nPontuacao Final' + '\033[0m')
-print('\033[33m' + f'Pontos do AI 1: {pontos_AI_1}')
-print(f'Pontos do Utilizador: {pontos_Utilizador}' + '\033[0m')
+print('\nPontuação Final: ')
+print(f'Pontos do AI 1: {PONTOS_AI_1}')
+print(f'Pontos do Utilizador: {PONTOS_UTILIZADOR}')
